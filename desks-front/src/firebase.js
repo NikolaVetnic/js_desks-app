6f0ae20b55f7c2c289b13ef9ff1da95133a30acd
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2023-06-08 23:58:35 +0200

    firebase config function

--- desks-front/src/firebase.js
+++ desks-front/src/firebase.js
@@ -1,5 +1,7 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/database";
+import "firebase/auth";
+import "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: "AIzaSyABr6uIzcw0CAJ-OHHc4umdV2ZUo5YLPpk",
