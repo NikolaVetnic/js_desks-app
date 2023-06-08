e42df925a33f62b6d2d818ace9537a3201f7732f
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2023-06-08 23:53:29 +0200

    component Footer

---
+++ desks-front/src/components/UI/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import "./Footer.css";
+
+const Footer = () => {
+    return (
+        <footer className="footer pt-3">
+            <div className="container">
+                <div className="footer-content">
+                    <p>
+                        &copy; 2023 j&s-soft Desk Tracking App. Initial version
+                        by j&s-soft Novi Sad.
+                    </p>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
