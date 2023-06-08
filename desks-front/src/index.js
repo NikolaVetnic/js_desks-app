5f6dc1626ce242578961e3c159cdebd2832ecd3b
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2023-06-08 23:58:45 +0200

    router and modal added

--- desks-front/src/index.js
+++ desks-front/src/index.js
@@ -4,10 +4,16 @@ import "./index.css";
 import App from "./App";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
+import ReactModal from "react-modal";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+ReactModal.setAppElement("#root"); // Set the app element to the root of your application
+
 root.render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </React.StrictMode>
 );
