857825ab7e26dc556a31350841681396ef646ec2
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2023-06-08 23:56:08 +0200

    component Dashboard

---
+++ desks-front/src/components/Pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,19 @@
+import verifyToken from "../../../utils/verifyToken";
+import AdminDashboard from "./AdminDashboard";
+import UserDashboard from "./UserDashboard";
+
+const Dashboard = () => {
+    const { username, role } = verifyToken();
+
+    return (
+        <div className="container">
+            {role === "admin" ? (
+                <AdminDashboard username={username} />
+            ) : (
+                <UserDashboard username={username} />
+            )}
+        </div>
+    );
+};
+
+export default Dashboard;
