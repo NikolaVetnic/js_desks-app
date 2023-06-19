b6e666fac6cc58bbbcbfeb0a680eff8db9761017
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2023-06-19 08:46:22 +0200

    location sel dropdown refactored to DeskContainer

--- desks-front/src/components/Pages/Dashboard/UserDashboard.jsx
+++ desks-front/src/components/Pages/Dashboard/UserDashboard.jsx
@@ -1,30 +1,11 @@
-import { useState } from "react";
-import DeskContainer from "../../UI/Desk/DeskContainer";
+import DeskContainer from "../../UI/Desk/DeskContainer/DeskContainer";
 
 const Dashboard = ({ username }) => {
-    const [location, setLocation] = useState("");
-
-    const handleLocationChange = (event) => {
-        setLocation(event.target.value);
-        console.log(`location changed to ${event.target.value}`);
-    };
-
     return (
         <div className="container">
             <h1>USER Dashboard</h1>
             <p>Currently logged in as : {username}</p>
-
-            <div>
-                Chosen location : &nbsp;
-                <select value={location} onChange={handleLocationChange}>
-                    <option value="">- choose location -</option>
-                    <option value="hd">Heidelberg</option>
-                    <option value="md">Magdeburg</option>
-                    <option value="ns">Novi Sad</option>
-                </select>
-            </div>
-
-            <div>{!!location ? <DeskContainer location={location} /> : ""}</div>
+            <DeskContainer />
         </div>
     );
 };
