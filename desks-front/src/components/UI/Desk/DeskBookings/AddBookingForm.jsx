1be6695bf4108f9c7e5379c0e3a8dc2d284f86a7
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2023-06-21 12:57:07 +0200

    AddBookingForm v2, heavy use of Formik & Yup

--- desks-front/src/components/UI/Desk/DeskBookings/AddBookingForm.jsx
+++ desks-front/src/components/UI/Desk/DeskBookings/AddBookingForm.jsx
@@ -1,38 +1,89 @@
-import React from "react";
-import { Formik, Form, Field, ErrorMessage, TimePicker } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { useNavigate } from "react-router-dom";
 import * as Yup from "yup";
-import addBookingHandleSubmit from "./addBookingHandleSubmit";
+
+import addBookingToDesk from "../../../../utils/db/addBookingToDesk";
+import {
+    generateTimeOptions,
+    isNotOverlappingWithExistingInterval,
+    isTimeToAfterTimeFrom,
+    parseTime,
+} from "../../../../utils/timeUtils";
+import getDeskFromDb from "../../../../utils/db/getDeskFromDb";
+import verifyToken from "../../../../utils/verifyToken";
 
 const validationSchema = Yup.object().shape({
-    room: Yup.number().required("Room is required"),
-    desk: Yup.number().required("Desk is required"),
-    date: Yup.date().required("Date is required"),
-    timeFrom: Yup.string().required("Start time is required"),
-    timeTo: Yup.string().required("End time is required"),
-    bookedBy: Yup.string().email("Invalid email").required("Email is required"),
+    date: Yup.date()
+        .required("Date is required")
+        .test("is-weekend", "Weekend days are not allowed", (value) => {
+            const dayOfWeek = new Date(value).getDay();
+            return dayOfWeek !== 0 && dayOfWeek !== 6;
+        }),
+    timeFrom: Yup.string().required("Time From is required"),
+    timeTo: Yup.string()
+        .required("Time To is required")
+        .test("is-after", "Time To must be after Time From", function (value) {
+            const { timeFrom } = this.parent;
+            return isTimeToAfterTimeFrom(timeFrom, value);
+        }),
 });
 
-const AddBookingForm = (prop) => {
-    // TODO: [JSSBG-18] only display form if there are time slots available during the day
+const AddBookingForm = ({ location, room, desk }) => {
+    const navigate = useNavigate();
+
+    const username = verifyToken().username;
+
+    // if user is not logged in the username will be undefined, redirect to login page
+    if (!username) {
+        navigate("/login");
+        return;
+    }
+
+    const initialValues = {
+        location,
+        room,
+        desk,
+        date: "",
+        timeFrom: "",
+        timeTo: "",
+        bookedBy: verifyToken().username,
+    };
+
+    const handleSubmit = async ({ date, timeFrom, timeTo }) => {
+        const dataToSave = { ...initialValues, date, timeFrom, timeTo };
+
+        const filteredDesks = await getDeskFromDb(location, room, desk);
+
+        const isOverlapping = filteredDesks.bookings
+            ? Object.values(filteredDesks.bookings).some((booking) => {
+                  return !isNotOverlappingWithExistingInterval(
+                      timeFrom,
+                      timeTo,
+                      booking.timeFrom,
+                      booking.timeTo
+                  );
+              })
+            : false;
+
+        if (isOverlapping) {
+            console.log(
+                "one of the user's bookings is overlapping with the new booking"
+            );
+            return;
+        }
+
+        addBookingToDesk(filteredDesks.key, dataToSave);
+    };
 
     return (
         <div>
-            <h1>Add Booking</h1>
+            <h1>Add Booking Form</h1>
             <Formik
-                initialValues={{
-                    room: prop.room,
-                    desk: prop.desk,
-                    date: "",
-                    timeFrom: "",
-                    timeTo: "",
-                    bookedBy: "",
-                    location: prop.location
-                }}
+                initialValues={initialValues}
                 validationSchema={validationSchema}
-                onSubmit={addBookingHandleSubmit}
+                onSubmit={handleSubmit}
             >
-                <Form id="formik-form">
-
+                <Form>
                     <div className="form-group">
                         <label htmlFor="date">Date:</label>
                         <Field
@@ -49,49 +100,44 @@ const AddBookingForm = (prop) => {
                     </div>
 
                     <div className="form-group">
-                        <label htmlFor="timeFrom">Start Time:</label>
-                        <Field
-                            type="time"
-                            id="timeFrom"
-                            name="timeFrom"
-                            className="form-control"    
-                            
-                        />
-                        <ErrorMessage
-                            name="timeFrom"
-                            component="div"
-                            className="text-danger"
-                        />
-                    </div>
-
-                    <div className="form-group">
-                        <label htmlFor="timeTo">End Time:</label>
-                        <Field
-                            type="time"
-                            id="timeTo"
-                            name="timeTo"
-                            className="form-control"
-                        />
-                        <ErrorMessage
-                            name="timeTo"
-                            component="div"
-                            className="text-danger"
-                        />
-                    </div>
-
-                    <div className="form-group">
-                        <label htmlFor="bookedBy">Booked By:</label>
-                        <Field
-                            type="email"
-                            id="bookedBy"
-                            name="bookedBy"
-                            className="form-control"
-                        />
-                        <ErrorMessage
-                            name="bookedBy"
-                            component="div"
-                            className="text-danger"
-                        />
+                        <div className="row">
+                            <div className="col">
+                                <label htmlFor="timeFrom">Time From:</label>
+                                <Field
+                                    as="select"
+                                    id="timeFrom"
+                                    name="timeFrom"
+                                    className="form-control"
+                                >
+                                    <option value="">
+                                        - Select Time From -
+                                    </option>
+                                    {generateTimeOptions()}
+                                </Field>
+                                <ErrorMessage
+                                    name="timeFrom"
+                                    component="div"
+                                    className="text-danger"
+                                />
+                            </div>
+                            <div className="col">
+                                <label htmlFor="timeTo">Time To:</label>
+                                <Field
+                                    as="select"
+                                    id="timeTo"
+                                    name="timeTo"
+                                    className="form-control"
+                                >
+                                    <option value="">- Select Time To -</option>
+                                    {generateTimeOptions()}
+                                </Field>
+                                <ErrorMessage
+                                    name="timeTo"
+                                    component="div"
+                                    className="text-danger"
+                                />
+                            </div>
+                        </div>
                     </div>
 
                     <button type="submit" className="btn btn-primary">
