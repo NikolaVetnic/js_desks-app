75e6bf6c04ee1f66abd48851ae71118987734c91
Vladimir Vuckovic <vidrak97@gmail.com>
2023-06-26 12:13:28 +0200

    desks are now displayed properly and they are fetched from db

--- desks-front/src/components/UI/Desk/DeskContainer/DeskContainer.jsx
+++ desks-front/src/components/UI/Desk/DeskContainer/DeskContainer.jsx
@@ -1,89 +1,87 @@
 import { useEffect, useState } from "react";
-
 import { Button, ButtonToolbar, OverlayTrigger } from "react-bootstrap";
 import "./DeskContainer.css";
 import { AvailableTooltip } from "../../Tooltip/Tooltips";
 import DropdownMenu from "../../DropdownMenu";
 import SelectedDeskDisplay from "../SelectedDeskDisplay";
-
-// temporary data, should be fetched from firebase
-const desks = [
-    { location: "hd", room: 1, desk: 1, addedBy: "admin@admin.com" },
-    { location: "ns", room: 1, desk: 1, addedBy: "admin@admin.com" },
-    { location: "ns", room: 1, desk: 5, addedBy: "admin@admin.com" },
-    { location: "ns", room: 1, desk: 2, addedBy: "admin@admin.com" },
-    { location: "md", room: 1, desk: 5, addedBy: "admin@admin.com" },
-    { location: "md", room: 2, desk: 2, addedBy: "admin@admin.com" },
-];
+import getDesksByLocationFromDb from "../../../../utils/db/getDesksByLocationFromDb";
 
 const menuOptions = [
-    { value: "", label: "- choose option -" },
-    { value: "hd", label: "Heidelberg" },
-    { value: "md", label: "Magdeburg" },
-    { value: "ns", label: "Novi Sad" },
+  { value: "", label: "- choose option -" },
+  { value: "hd", label: "Heidelberg" },
+  { value: "md", label: "Magdeburg" },
+  { value: "ns", label: "Novi Sad" },
 ];
 
 const DeskContainer = () => {
-    const [location, setLocation] = useState("");
+  const [location, setLocation] = useState("");
+  const [desks, setDesks] = useState([]);
+  const [selectedDesk, setSelectedDesk] = useState(null);
 
-    const handleLocationChange = (event) => {
-        setLocation(event.target.value);
-        setSelectedDesk(null);
-        console.log(`location changed to ${event.target.value}`);
-    };
+  const handleLocationChange = (event) => {
+    setLocation(event.target.value);
+    setSelectedDesk(null);
+    console.log(`Location changed to ${event.target.value}`);
+  };
 
-    //selected desk variable to change on button clicked
-    const [selectedDesk, setSelectedDesk] = useState(null);
+  useEffect(() => {
+    handleDesks();
+  }, [location]);
 
-    useEffect(() => {
-        if (selectedDesk !== null) {
-            console.log(selectedDesk);
-        }
-    }, [selectedDesk]);
+  const handleDesks = async () => {
+    // TODO: [JSSBG-28] display real desks from database
+    try {
+      const result = await getDesksByLocationFromDb(location);
+      const desksData = result || [];
+      setDesks(desksData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    const handleModalOpen = (item) => {
-        setSelectedDesk(item);
-    };
+  const handleModalOpen = (item) => {
+    setSelectedDesk(item);
+  };
 
-    const positionerInstance = (item) => {
-        return (
-            <ButtonToolbar>
-                <OverlayTrigger placement="top" overlay={AvailableTooltip}>
-                    <Button
-                        onClick={() => handleModalOpen(item)}
-                        variant="success"
-                        className="flexbox-item"
-                    ></Button>
-                </OverlayTrigger>
-            </ButtonToolbar>
-        );
-    };
-
+  const positionerInstance = (item) => {
     return (
-        <>
-            <DropdownMenu
-                value={location}
-                options={menuOptions}
-                onChange={handleLocationChange}
-            />
-
-            <div className="flexbox-container mt-3">
-                <div className="rowChild">
-                    {desks.map((item, index) => {
-                        if (location === item.location) {
-                            return (
-                                <div key={index}>
-                                    {positionerInstance(item)}
-                                </div>
-                            );
-                        }
-                    })}
-                </div>
-            </div>
-
-            {!!selectedDesk && <SelectedDeskDisplay {...selectedDesk} />}
-        </>
+      <ButtonToolbar>
+        <OverlayTrigger placement="top" overlay={AvailableTooltip}>
+          <Button
+            onClick={() => handleModalOpen(item)}
+            variant="success"
+            className="flexbox-item"
+          ></Button>
+        </OverlayTrigger>
+      </ButtonToolbar>
     );
+  };
+
+  return (
+    <>
+      <DropdownMenu
+        value={location}
+        options={menuOptions}
+        onChange={handleLocationChange}
+      />
+
+      <div className="flexbox-container mt-3">
+        <div className="rowChild">
+          {location && desks.map((item, index) => {
+            if (location === item.location) {
+              return (
+                <div key={index}>
+                  {positionerInstance(item)}
+                </div>
+              );
+            }
+          })}
+        </div>
+      </div>
+
+      {!!selectedDesk && <SelectedDeskDisplay {...selectedDesk} />}
+    </>
+  );
 };
 
 export default DeskContainer;
