394db22f7d3a4498a64bb46d360fcdcf7f4659a4
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2023-06-29 09:16:48 +0200

    chore: newline removed

--- desks-front/src/components/UI/Desk/UserBookings/UserBookingsDisplay.jsx
+++ desks-front/src/components/UI/Desk/UserBookings/UserBookingsDisplay.jsx
@@ -1,18 +1,17 @@
 import DeskBookingsDisplay from "../DeskBookings/DeskBookingsDisplay";
 
-
-const UserBookingsDisplay = ({bookings}) => {
-    if (!Array.isArray(bookings) || bookings.length === 0 ) {
+const UserBookingsDisplay = ({ bookings }) => {
+    if (!Array.isArray(bookings) || bookings.length === 0) {
         return <div>No bookings found.</div>;
-      }
-    
-      return (
+    }
+
+    return (
         <div>
-          {bookings.map((booking, index) => (
-            <DeskBookingsDisplay key={index} booking={booking} />
-          ))}
+            {bookings.map((booking, index) => (
+                <DeskBookingsDisplay key={index} booking={booking} />
+            ))}
         </div>
-      );
-};    
+    );
+};
 
 export default UserBookingsDisplay;
