a0a8968ff86c9ecfd1fc367454c0add9a3f4f272
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-07-03 10:43:23 +0200

    Dummy data removed

--- desks-front/src/components/UI/Desk/DeskBookings/DeskBookingsDisplay.jsx
+++ desks-front/src/components/UI/Desk/DeskBookings/DeskBookingsDisplay.jsx
@@ -1,33 +1,8 @@
-import { formatDate } from "../../../../utils/timeUtils";
 import BookingsTableView from "./BookingsTableView";
+import { getBookings } from "../../../../utils/db/getBookingsForDesk";
 
 // TODO: [JSSBG-26] this is just a dummy data - should be replaced with real data from the backend
-const bookings = [
-  {
-    room: 1,
-    desk: 2,
-    date: "2022-05-25",
-    timeFrom: "09:00",
-    timeTo: "12:00",
-    bookedBy: "user@example.com",
-  },
-  {
-    room: 3,
-    desk: 4,
-    date: "2022-06-25",
-    timeFrom: "10:00",
-    timeTo: "13:00",
-    bookedBy: "example@user.com",
-  },
-  {
-    room: 3,
-    desk: 4,
-    date: "2022-06-25",
-    timeFrom: "13:00",
-    timeTo: "13:30",
-    bookedBy: "example@user.com",
-  },
-];
+const bookings =await getBookings();
 
 const DeskBookingsDisplay = () => {
   return (
