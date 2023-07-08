275b4d9cbc955da606fd76fe28ab327e7491b533
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2023-07-08 11:49:25 +0200

    backup firebase config added

--- desks-front/src/firebase.js
+++ desks-front/src/firebase.js
@@ -3,7 +3,10 @@ import "firebase/compat/database";
 import "firebase/auth";
 import "firebase/firestore";
 
-const firebaseConfig = {
+// FLIP THIS to change the database used
+const useOriginal = false;
+
+const firebaseConfig0 = {
     apiKey: "AIzaSyABr6uIzcw0CAJ-OHHc4umdV2ZUo5YLPpk",
     authDomain: "js-desks.firebaseapp.com",
     databaseURL:
@@ -14,7 +17,18 @@ const firebaseConfig = {
     appId: "1:738947568452:web:882f57627067f9743dbd11",
 };
 
+const firebaseConfig1 = {
+    apiKey: "AIzaSyDgouOn47qxm_f4UUczNTmP3VYWgNmOoEg",
+    authDomain: "js-desks-4b7aa.firebaseapp.com",
+    databaseURL: "https://js-desks-4b7aa-default-rtdb.firebaseio.com",
+    projectId: "js-desks-4b7aa",
+    storageBucket: "js-desks-4b7aa.appspot.com",
+    messagingSenderId: "451039070934",
+    appId: "1:451039070934:web:83a4a208dac9a3e1401777",
+    measurementId: "G-8QPQ7GXQZD",
+};
+
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(useOriginal ? firebaseConfig0 : firebaseConfig1);
 
 export default firebase;
