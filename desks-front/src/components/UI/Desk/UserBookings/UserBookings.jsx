1f52baae230c22c611ee5925990d71b72a02488f
Vladimir Vuckovic <vidrak97@gmail.com>
2023-07-28 12:34:11 +0200

    created new UserBookings component

---
+++ desks-front/src/components/UI/Desk/UserBookings/UserBookings.jsx
@@ -0,0 +1,30 @@
+import { useCallback, useEffect, useState } from "react";
+import verifyToken from "../../../../utils/verifyToken";
+
+import getBookingsForUser from "../../../../utils/db/getBookingsForUser";
+import BookingsTableView from "../DeskBookings/BookingsTableView";
+
+export const UserBookings = () => {
+    const [bookings, setBookings] = useState(null);
+
+    const handleBookingAdded = useCallback(async () => {
+        try{
+            const updatedBookings = await getBookingsForUser(verifyToken().username);
+            setBookings(updatedBookings);
+        }catch (error) {
+            console.log(error);
+        }
+    }, []);
+
+
+     useEffect(() => {
+        handleBookingAdded()
+    }, [ handleBookingAdded ]);
+
+    return (
+        <>
+            <p>Booked by me:</p>
+            <BookingsTableView bookings={bookings}/>
+        </>
+    )
+}
